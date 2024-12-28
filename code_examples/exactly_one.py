b8e20025e36c861df6f9b708337803b5568b5a01
Dinar5150 <d.yakupov@innopolis.university>
2024-12-28 22:51:23 +0300

    Initial commit

---
+++ code_examples/exactly_one.py
@@ -0,0 +1,10 @@
+from dwave.system import DWaveSampler, EmbeddingComposite
+sampler = EmbeddingComposite(DWaveSampler(token="DEV-..."))
+
+linear = {('a', 'a'): -1, ('b', 'b'): -1, ('c', 'c'): -1}
+quadratic = {('a', 'b'): 2, ('b', 'c'): 2, ('a', 'c'): 2}
+Q = {**linear, **quadratic}
+
+sampleset = sampler.sample_qubo(Q, num_reads=500)
+
+print(sampleset)
