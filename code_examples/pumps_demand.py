e7aa1dd34e24a3e095a54250ad9ac405e8948e84
Dinar5150 <d.yakupov@innopolis.university>
2024-12-29 17:01:59 +0300

    Update, put higher costs, figured out how to manage int8 overflow

--- code_examples/pumps_demand.py
+++ code_examples/pumps_demand.py
@@ -1,35 +1,36 @@
 from dimod import BinaryQuadraticModel
+from dimod import make_quadratic
 from dwave.samplers import SimulatedAnnealingSampler
+import numpy as np
 
-bqm = BinaryQuadraticModel("BINARY")
+bqm = BinaryQuadraticModel("BINARY")  # Specify dtype to handle larger numbers
 
-pumps = [0, 1, 2, 3] # Pump IDs
+pumps = [0, 1, 2, 3]  # Pump IDs
 costs = [
-    [36, 27],
-    [56, 65],
-    [48, 36],
-    [52, 16]
-] # Costs to pump with different pumps at different times
-flow = [2, 7, 3, 8] # Flows for 4 pumps
-demand = 20 # The demand for the pumps that need to be satisfied (equality)
-time = [0, 1] # AM and PM separation
+    [136, 227],
+    [156, 165],
+    [148, 136],
+    [152, 216]
+]  # Costs to pump with different pumps at different times
+flow = [2, 7, 3, 8]  # Flows for 4 pumps
+demand = 20  # The demand for the pumps that need to be satisfied (equality)
+time = [0, 1]  # AM and PM separation
 
-
-x = [[f"P{p}_AM", f"P{p}_PM"] for p in pumps] # Variable names for each pump timeslot
+x = [[f"P{p}_AM", f"P{p}_PM"] for p in pumps]  # Variable names for each pump timeslot
 
 for p in pumps:
     for t in time:
-        bqm.add_variable(x[p][t], costs[p][t]) # Create a variable with variable name at x[p][t] with bias equal to its cost
+        bqm.add_variable(x[p][t], costs[p][t])  # Create a variable with variable name at x[p][t] with bias equal to its cost
 
 # Each pump is run at least once and no more than 'len(time)' times
 for p in pumps:
-    c1 = [(x[p][t], 1) for t in time] # Initialize contraint with variable name and bias for it in the constraint inequality
+    c1 = [(x[p][t], 1) for t in time]  # Initialize constraint with variable name and bias for it in the constraint inequality
     bqm.add_linear_inequality_constraint(
-        c1, # Put the constraints with their biases, this is used to calculate weighted sum
-        lb = 1, # Each pump is occupied at least once a day
-        ub = len(time), # Each pump can't be occupied for more than the whole day
-        lagrange_multiplier = 5, # Penalty factor
-        label = "c1_pump_" + str(p)
+        c1,  # Put the constraints with their biases, this is used to calculate weighted sum
+        lb=1,  # Each pump is occupied at least once a day
+        ub=len(time),  # Each pump can't be occupied for more than the whole day
+        lagrange_multiplier=5,  # Penalty factor
+        label="c1_pump_" + str(p)
     )
 
 # At most 'len(pumps)' pumps at 1 time
@@ -37,21 +38,21 @@ for t in time:
     c2 = [(x[p][t], 1) for p in pumps]
     bqm.add_linear_inequality_constraint(
         c2,
-        constant = -len(pumps),
-        lagrange_multiplier = 1,
-        label = "c1_time_" + str(t)
+        constant=-len(pumps),
+        lagrange_multiplier=1,
+        label="c1_time_" + str(t)
     )
 
 # Satisfy demand
 c3 = [(x[p][t], flow[p]) for p in pumps for t in time]
 bqm.add_linear_equality_constraint(
     c3,
-    constant = -demand,
-    lagrange_multiplier = 28
+    constant=-demand,
+    lagrange_multiplier=28
 )
 
 sampler = SimulatedAnnealingSampler()
-sampleset = sampler.sample(bqm, num_reads = 10000)
+sampleset = sampler.sample(bqm, num_reads=1000)
 
 sample = sampleset.first.sample
 
@@ -62,13 +63,13 @@ print("\n\tAM\tPM")
 for p in pumps:
     printout = 'P' + str(p)
     for t in time:
-        sample_val = sample[x[p][t]]
+        sample_val = int(sample[x[p][t]])
         flow_val = flow[p]
-        cost_val = costs[p][t] # Debugging mess
+        cost_val = costs[p][t]  # Debugging mess
 
         printout += '\t' + str(sample_val)
-        total_flow += int(sample_val * flow_val)
-        total_cost += int(sample_val * cost_val)
+        total_flow += sample_val * flow_val
+        total_cost += sample_val * cost_val
     print(printout)
 
 print("Total flow:", total_flow)
